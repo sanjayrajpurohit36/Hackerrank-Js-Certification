e6a9bc58549cd8ca6afeff3ee20a95ae711d9779
Sanjay Rajpurohit <sanjay.rajpurohit@paytmmoney.com>
2021-12-24 21:32:16 +0530

    first commit

---
+++ Parkinglot.js
@@ -0,0 +1,48 @@
+/*
+
+
+
+
+*/
+class ParkingLot {
+    // Add the methods here
+    constructor(slots) {
+        this.slots = new Array(slots);
+        slots.fill(0);
+    }
+    
+    park(carId) {
+        for(let i = 0; i<this.slots.length; i++){
+            if(this.slots[i].length == 0) {
+                this.slots[i].push(carId);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    getSlots() {
+        let ans = [];
+        for(let i = 0; i<this.slots.length; i++){ 
+            if(this.slots[i].length) {
+                ans.push(this.slots[i]);
+            } else ans.push(null);
+        }
+        // console.log("=>", ans);
+        return ans;
+    }
+    
+    remove(carId) {
+        for(let i = 0; i<this.slots.length; i++){ 
+            if(this.slots[i][0] == carId) {
+                this.slots[i] = [];
+                return true;
+            } 
+        }
+        return false;
+    }
+
+}
+
+
+let ParkinglotObject = new ParkingLot;
